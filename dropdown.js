9fbb6f173460583c3e97ac28dc937a1caefed511
jrioux123 <johnrioux98@gmail.com>
2017-08-19 23:51:40 -0400

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -27,12 +27,12 @@ function popupFunction() {
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.popbtn')) {
-    var dropdowns = document.getElementsByClassName("popup-content");
+    var popups = document.getElementsByClassName("popup-content");
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+    for (i = 0; i < popups.length; i++) {
+      var openPopup = popups[i];
+      if (openPopup.classList.contains('show')) {
+        openPopup.classList.remove('show');
       }
     }
   }
