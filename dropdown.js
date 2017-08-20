ae17525fe6616b63a937a436c651141e1a9c48ce
jrioux123 <johnrioux98@gmail.com>
2017-08-19 23:56:23 -0400

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -16,25 +16,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-/* Here's the same thing, but for popups instead! */
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function popupFunction() {
-    document.getElementById("myPopup").classList.toggle("show");
-}
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.popbtn')) {
-    var popups = document.getElementsByClassName("popup-content");
-    var i;
-    for (i = 0; i < popups.length; i++) {
-      var openPopup = popups[i];
-      if (openPopup.classList.contains('show')) {
-        openPopup.classList.remove('show');
-      }
-    }
-  }
-}
-
