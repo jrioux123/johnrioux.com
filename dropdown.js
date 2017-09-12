f056436cf3fbe87f34ca5632f3ec9d683300f03d
jrioux123 <johnrioux98@gmail.com>
2017-09-12 18:58:00 -0400

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -40,5 +40,3 @@ function dialogKill() {
     var x = document.getElementById('dialogID');
     x.style.display = 'none';
 }
-
-document.getElementById("tweetID").innerHTML = "<a href=https://twitter.com/intent/tweet?status=" + window.location.href + ">Click here</a>";
