8298ececf7ab569aeabb7d15cc28a393881ee076
jrioux123 <johnrioux98@gmail.com>
2018-06-14 18:19:05 -0400

    Create googlecustomsearch.js

---
+++ googlecustomsearch.js
@@ -0,0 +1,9 @@
+  (function() {
+    var cx = '016891107299133920326:wwaszvag6gw';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
