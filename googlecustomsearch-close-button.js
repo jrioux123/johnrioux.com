0e2c7878653417c5d1e5cadaac351c9e4fecad4b
jrioux123 <johnrioux98@gmail.com>
2018-06-15 15:24:36 -0400

    Delete googlecustomsearch-close-button.js

--- googlecustomsearch-close-button.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("gs_cb50").innerHTML = "close";
-document.getElementById("gs_cb50").classList.add("material-icons");
-document.getElementById("gs_cb50").classList.add("exitsearch");
