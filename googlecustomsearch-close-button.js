63e6eef29141c0ced02b961b6fe38f54e53e94c9
jrioux123 <johnrioux98@gmail.com>
2018-06-14 22:16:27 -0400

    Update googlecustomsearch-close-button.js

--- googlecustomsearch-close-button.js
+++ googlecustomsearch-close-button.js
@@ -1,1 +1,3 @@
-    uh
+document.getElementById("gs_cb50").innerHTML = "close";
+document.getElementById("gs_cb50").classList.add("material-icons");
+document.getElementById("gs_cb50").classList.add("exitsearch");
