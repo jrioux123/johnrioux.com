5c0c04be07a99bdf40f1d2533a1c56eda5e9258f
jrioux123 <johnrioux98@gmail.com>
2018-06-14 21:47:35 -0400

    Update googlecustomsearch.js

--- googlecustomsearch.js
+++ googlecustomsearch.js
@@ -8,7 +8,3 @@
     s.parentNode.insertBefore(gcse, s);
   })();
 
- 
-    document.getElementById("gs_cb50").innerHTML = "close";
-    document.getElementById("gs_cb50").classList.add("material-icons");
-    document.getElementById("gs_cb50").classList.add("exitsearch");
