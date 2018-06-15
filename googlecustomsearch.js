5f4f4489e945afa348b9bea671311e278f1df10f
jrioux123 <johnrioux98@gmail.com>
2018-06-14 21:30:29 -0400

    Added some changes to the X button element

--- googlecustomsearch.js
+++ googlecustomsearch.js
@@ -7,3 +7,8 @@
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
   })();
+
+ 
+    document.getElementById("gs_cb50").innerHTML = "close";
+    document.getElementById("gs_cb50").classList.add("material-icons");
+    document.getElementById("gs_cb50").classList.add("exitsearch");
