4c15357f64ce08ad5db1439b08b856544a511642
jrioux123 <johnrioux98@gmail.com>
2018-06-18 00:28:48 -0400

    Update googlecustomsearch.js

--- googlecustomsearch.js
+++ googlecustomsearch.js
@@ -2,6 +2,7 @@
     var cx = '016891107299133920326:wwaszvag6gw';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
+    gcse.async = true;
     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
