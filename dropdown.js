965a770dc842554bc68b52f236d2ba028b6217b1
jrioux123 <johnrioux98@gmail.com>
2018-09-09 21:01:11 -0400

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -31,22 +31,12 @@ function shadeKill() {
     x.style.display = 'none';
 }
 
-function shareCreate() {
-    var x = document.getElementById('shareID')
+function dialogCreate() {
+    var x = document.getElementById('dialogID')
     x.style.display = 'block';
 }
 
-function shareKill() {
-    var x = document.getElementById('shareID');
-    x.style.display = 'none';
-}
-
-function followCreate() {
-    var x = document.getElementById('followID')
-    x.style.display = 'block';
-}
-
-function followKill() {
-    var x = document.getElementById('followID');
+function dialogKill() {
+    var x = document.getElementById('dialogID');
     x.style.display = 'none';
 }
