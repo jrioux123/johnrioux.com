d00a071ff375a7c05ee93e5922e3e255a964845d
jrioux123 <johnrioux98@gmail.com>
2018-09-09 21:57:43 -0400

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -40,30 +40,3 @@ function dialogKill() {
     var x = document.getElementById('dialogID');
     x.style.display = 'none';
 }
-
-// Here's some maybe dangerous garbage:
-
-    var dialogIndex = 1;
-    showDialog(dialogIndex);
-    
-    function currentDialog(n) {
-      showDialog(dialogIndex = n);
-    }
-
-    
-    function showDialog(n) {
-      var i;
-      var dialog = document.getElementsByClassName("dialog-indexer");
-      var dialogButtons = document.getElementsByClassName("dialogButton");
-      if (n > dialog.length) {dialogIndex = 1}    
-      if (n < 1) {dialogIndex = dialog.length}
-      for (i = 0; i < dialog.length; i++) {
-          dialog[i].style.display = "none";  
-      }
-      for (i = 0; i < dialogButtons.length; i++) {
-          dialogButtons[i].className = dialogButtons[i].className.replace(" active", "");
-      }
-      dialog[dialogIndex-1].style.display = "block";  
-      diaogButtons[dialogIndex-1].className += " active";
-    }
-
