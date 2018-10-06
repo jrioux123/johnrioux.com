9f22d023d805ed564a0c3318c14ae1df5162050c
jrioux123 <johnrioux98@gmail.com>
2018-10-06 16:56:01 -0400

    Update onload.js

--- onload.js
+++ onload.js
@@ -3,4 +3,11 @@ function onloadJS() {
     document.getElementsByName("search")[0].placeholder="Search johnrioux.net";
     document.querySelector('input').autofocus = true;   
     document.querySelector('input').onblur="this.focus()";
+    document.getElementByName('search').onblur = function (event) { 
+        var blurEl = this; 
+        setTimeout(function() {
+            blurEl.focus()
+        }, 10);
+    };
 }
+
