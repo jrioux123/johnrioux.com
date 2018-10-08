d3c250c199820566f2d79394a77a034f19efa7b2
jrioux123 <johnrioux98@gmail.com>
2018-10-08 11:57:27 -0400

    Update onload.js

--- onload.js
+++ onload.js
@@ -1,13 +1,6 @@
 function onloadJS() {
     document.getElementsByName("search")[0].value="";
     document.getElementsByName("search")[0].placeholder="Search johnrioux.net";
-    document.querySelector('input').autofocus = true;   
-    document.querySelector('input').onblur="this.focus()";
-    document.getElementById('gsc-i-id1').onblur = function (event) { 
-        var blurEl = this; 
-        setTimeout(function() {
-            blurEl.focus()
-        }, 10);
-    };
+    document.querySelector('input').autofocus = true;
 }
 
