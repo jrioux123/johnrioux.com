23068c17c5d82a7af39b9166c16355f883de2de9
jrioux123 <johnrioux98@gmail.com>
2019-02-08 02:24:31 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,23 +1,6 @@
 // Ajax
 
-$(document).ready(function(){
-    $("#navigation").load(function(){
-        $.ajax({url: "navigation.html", success: function() {
-            $(".tabsbar").delegate("button", "click", function() {
-                _link = $(this).attr("href");
-                _name = $(this).attr("data-name")
-                history.pushState(null, null, _link);
-                $('#contentloader').load(_link + ' #pagecontent');
-                document.title = "John Rioux | " + _name;
-                return false;
-            });
-            $(window).bind("popstate", function() {
-                _link = location.pathname.replace(/^.*[\\/]/, "");
-                $('#contentloader').load(_link + ' #pagecontent');
-            });
-        }});
-    });
-});
+$("#navigation").load("navigation.html);
 
 
 // Animations
