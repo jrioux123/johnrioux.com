efa15e18d4c1b7e8e1a84775824d7bfd277bc84c
jrioux123 <johnrioux98@gmail.com>
2019-04-26 15:07:42 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,6 +10,7 @@ $("#pagecontent").delegate("button", "click", function() {
     document.getElementById("menubutton").style.cssText = "display: none;";
     document.getElementById("backbutton").style.cssText = "display: block;";
     document.getElementById("searchbutton").style.cssText = "display: block;";
+    document.getElementById("searchbuttonlong").style.cssText = "display: none;";
     _link = $(this).attr("href");
     _name = $(this).attr("data-name");
     history.pushState(null, null, _link);
@@ -24,6 +25,7 @@ $(window).bind("popstate", function() {
     document.getElementById("backbutton").style.cssText = "display: none;";
     document.getElementById("menubutton").style.cssText = "display: block;";
     document.getElementById("searchbutton").style.cssText = "display: none";
+    document.getElementById("searchbuttonlong").style.cssText = "display: block";
     _link = location.pathname.replace(/^.*[\\/]/, "");
     $('#contentloader').load(_link + ' #pagecontent');
 });
@@ -114,13 +116,15 @@ function scrollFunction() {
         document.getElementById("topbar").classList.add("scrolled");
         document.getElementById("logo").classList.add("scrolled");
         document.getElementById("menuID").classList.add("scrolled");
-        document.getElementById("searchbutton").classList.add("scrolled");
+        document.getElementById("searchbutton").classList.add("scrolled");        
+        document.getElementById("searchbuttonlong").classList.add("scrolled");
         document.getElementById("searchID").classList.add("scrolled");
     } else {
         document.getElementById("topbar").classList.remove("scrolled");
         document.getElementById("logo").classList.remove("scrolled");
         document.getElementById("menuID").classList.remove("scrolled");
         document.getElementById("searchbutton").classList.remove("scrolled");
+        document.getElementById("searchbuttonlong").classList.remove("scrolled");
         document.getElementById("searchID").classList.remove("scrolled");
     }
 }
