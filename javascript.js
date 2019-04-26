5f6691940e4039e45f42780f42b54895eadef61b
jrioux123 <johnrioux98@gmail.com>
2019-04-26 17:39:00 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -7,10 +7,6 @@ function goBack() {
 // Open a card
 $("#pagecontent").delegate("button", "click", function() {
     document.getElementById("pagecontent").style.cssText = "margin-top: -56px;"; // Transition animation
-    document.getElementById("menubutton").classList.add("hidden");
-    document.getElementById("backbutton").classList.remove("hidden");
-    document.getElementById("searchbutton").classList.remove("hidden");
-    document.getElementById("searchbuttonlong").classList.add("hidden");
     _link = $(this).attr("href");
     _name = $(this).attr("data-name");
     history.pushState(null, null, _link);
@@ -24,8 +20,6 @@ $(window).bind("popstate", function() {
     document.getElementById("pagecontent").style.cssText = "margin-top: 56px;"; // Transition animation
     document.getElementById("backbutton").classList.add("hidden");
     document.getElementById("menubutton").classList.remove("hidden");
-    document.getElementById("searchbutton").classList.add("hidden");
-    document.getElementById("searchbuttonlong").classList.remove("hidden");
     _link = location.pathname.replace(/^.*[\\/]/, "");
     $('#contentloader').load(_link + ' #pagecontent');
 });
