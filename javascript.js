25e195c718ffa6e967ef1748c9ef5d193d9f6e58
jrioux123 <johnrioux98@gmail.com>
2019-04-26 14:21:07 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -117,13 +117,13 @@ function scrollFunction() {
         document.getElementById("logo").classList.add("scrolled");
         document.getElementById("menuID").classList.add("scrolled");
         document.getElementById("searchbutton").classList.add("scrolled");
-        document.getElementById("searchId").classList.add("scrolled");
+        document.getElementById("searchID").classList.add("scrolled");
     } else {
         document.getElementById("topbar").classList.remove("scrolled");
         document.getElementById("logo").classList.remove("scrolled");
         document.getElementById("menuID").classList.remove("scrolled");
         document.getElementById("searchbutton").classList.remove("scrolled");
-        document.getElementById("searchId").classList.remove("scrolled");
+        document.getElementById("searchID").classList.remove("scrolled");
     }
 }
 
