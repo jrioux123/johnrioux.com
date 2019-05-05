496e6079dcf269699681154318d5baaf5ab897c1
jrioux123 <johnrioux98@gmail.com>
2019-05-05 15:35:03 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -97,7 +97,7 @@ function searchFocus() {
 
 // Image gallery
 
-$("document").on("click", ".galleryimage", function(){
+$(document).on("click", ".galleryimage", function(){
     _source = $(this).attr("src");
     document.getElementById('enlargedimage').src = _source;
     document.getElementById("enlargedwrapper").classList.remove("hidden");
